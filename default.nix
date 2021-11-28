3591ed1a36802e77cc09be4b9de8af3ea97a7efe
Alexander Bich <code@quyse.net>
2021-11-29 01:44:26 +0300

    initial commit

---
+++ default.nix
@@ -0,0 +1,113 @@
+{ pkgsFun ? import <nixpkgs>
+, pkgs ? pkgsFun {}
+, lib ? pkgs.lib
+, toolchain
+, windows ? toolchain.windows {}
+, fixedsFile ? ./fixeds.json
+, fixeds ? lib.importJSON fixedsFile
+}:
+
+rec {
+  windows_disk = { mitmproxyCert }: windows.runPackerStep {
+    disk = windows.initialDisk {};
+    extraMount = "work";
+    extraMountOut = false;
+    beforeScript = ''
+      mkdir work
+      ln -s ${sysmon} work/sysmon64.exe
+      ln -s ${./sysmon.xml} work/sysmon.xml
+      ln -s ${mitmproxyCert} work/mitmproxy-ca-cert.cer
+    '';
+    provisioners = [
+      {
+        type = "powershell";
+        inline = [
+          # install sysmon
+          ''D:\sysmon64.exe -accepteula -i D:\sysmon.xml''
+          # increase maximum log size for sysmon
+          ''$log = Get-WinEvent -ListLog Microsoft-Windows-Sysmon/Operational; $log.MaximumSizeInBytes = 1gb; $log.SaveChanges()''
+          # add mitmproxy cert as trusted
+          ''certutil -addstore root D:\mitmproxy-ca-cert.cer''
+        ];
+      }
+    ];
+  };
+
+  run_gateway = { mitmproxyCerts }: let
+    configuration = import ./gateway.nix {
+      inherit mitmproxyCerts;
+    };
+    inherit (pkgs.nixos configuration) config;
+    inherit (config.system.build) toplevel;
+  in pkgs.writeScript "run_gateway" ''
+    ${pkgs.qemu_kvm}/bin/qemu-system-x86_64 \
+      -name tracking_trackers_gateway \
+      -cpu host -enable-kvm \
+      -smp 4 \
+      -m 1G \
+      -virtfs local,path=/nix/store,security_model=none,mount_tag=store,readonly=on \
+      -virtfs local,path=$(readlink -f data),security_model=none,mount_tag=data \
+      -kernel ${toplevel}/kernel \
+      -initrd ${toplevel}/initrd \
+      -append "$(<${toplevel}/kernel-params) init=${toplevel}/init" \
+      -device virtio-net,netdev=n0,mac=00:16:3E:11:11:01 \
+      -netdev socket,id=n0,listen=127.0.0.1:8008 \
+      -device virtio-net,netdev=n1,mac=00:16:3E:11:11:02 \
+      -netdev user,id=n1
+  '';
+
+  run_windows = { mitmproxyCerts }: pkgs.writeScript "run_windows" ''
+    ${pkgs.qemu_kvm}/bin/qemu-img create -f qcow2 -b ${windows_disk {
+      mitmproxyCert = "${mitmproxyCerts}/mitmproxy-ca-cert.cer";
+    }} -F qcow2 hdd.img
+    ${pkgs.qemu_kvm}/bin/qemu-system-x86_64 \
+      -name tracking_trackers_windows \
+      -enable-kvm \
+      -pidfile pid.txt \
+      -smp 4,cores=4,threads=1,sockets=1 \
+      -m 4G \
+      -cpu qemu64,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
+      -vga virtio \
+      -rtc base=localtime \
+      -drive file=hdd.img,format=qcow2,discard=unmap,detect-zeroes=unmap,index=0 \
+      -usb -device usb-tablet \
+      -serial none -parallel none \
+      -device virtio-net,netdev=n0,mac=00:16:3E:11:12:01 \
+      -netdev socket,id=n0,connect=127.0.0.1:8008
+  '';
+
+  tools = { seed }: let
+    mitmproxyCerts = generateMitmproxyCerts {
+      inherit seed;
+    };
+  in pkgs.linkFarm "tools" [
+    {
+      name = "run_gateway";
+      path = run_gateway {
+        inherit mitmproxyCerts;
+      };
+    }
+    {
+      name = "run_windows";
+      path = run_windows {
+        inherit mitmproxyCerts;
+      };
+    }
+  ];
+
+  generateMitmproxyCerts = { seed }: pkgs.runCommandLocal "mitmproxy_certs_${toString seed}" {} ''
+    # client replay of empty file - the only (?) way to do one-off mitmproxy run
+    touch empty
+    ${pkgs.mitmproxy}/bin/mitmdump --set confdir=$out --no-server --client-replay empty
+  '';
+
+  sysmon = pkgs.fetchurl {
+    inherit (fixeds.fetchurl."https://live.sysinternals.com/Sysmon64.exe") url sha256 name;
+  };
+
+  touch = {
+    inherit sysmon;
+
+    autoUpdateScript = toolchain.autoUpdateFixedsScript fixedsFile;
+  };
+}
---
+++ gateway.nix
@@ -0,0 +1,136 @@
+{ mitmproxyCerts }:
+{ config, pkgs, ... }: let
+
+  virtioFs = attrs: {
+    fsType = "9p";
+    options = [ "trans=virtio" "version=9p2000.L" "msize=16777216" "cache=loose" ];
+  } // attrs;
+
+  ethInternal = "ens5";
+  ethExternal = "ens6";
+
+in {
+  imports = [ <nixpkgs/nixos/modules/profiles/qemu-guest.nix> ];
+  system.stateVersion = "21.05";
+
+  boot.loader.systemd-boot.enable = true;
+
+  fileSystems."/" = {
+    device = "none";
+    fsType = "tmpfs";
+    options = [
+      "defaults"
+      "size=1G"
+      "mode=755"
+    ];
+  };
+  fileSystems."/nix/store" = virtioFs {
+    device = "store";
+    neededForBoot = true;
+  };
+  fileSystems."/data" = virtioFs {
+    device = "data";
+    neededForBoot = true;
+  };
+
+  networking.hostName = "gateway";
+  networking.hostId = "57495297";
+
+  # perform as gateway
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  networking.firewall.extraCommands = ''
+    iptables -t nat -A POSTROUTING -o ${ethExternal} -j MASQUERADE
+    iptables -A FORWARD -i ${ethInternal} -o ${ethExternal} -j ACCEPT
+    iptables -A FORWARD -i ${ethExternal} -o ${ethInternal} -m state --state RELATED,ESTABLISHED -j ACCEPT
+    iptables -t nat -A PREROUTING -i ${ethInternal} -p tcp --dport 80 -j REDIRECT --to-port 8080
+    iptables -t nat -A PREROUTING -i ${ethInternal} -p tcp --dport 443 -j REDIRECT --to-port 8080
+  '';
+  networking.firewall.allowedTCPPorts = [8080];
+  # static IP for internal interface
+  networking.interfaces."${ethInternal}".ipv4.addresses = [
+    {
+      address = "10.5.5.1";
+      prefixLength = 24;
+    }
+  ];
+  # dhcp server for connected VMs
+  services.dhcpd4 = {
+    enable = true;
+    interfaces = ["${ethInternal}"];
+    extraConfig = ''
+      option subnet-mask 255.255.255.0;
+      option broadcast-address 10.5.5.255;
+      option routers 10.5.5.1;
+      option domain-name-servers 1.1.1.1;
+      subnet 10.5.5.0 netmask 255.255.255.0 {
+        range 10.5.5.100 10.5.5.200;
+      }
+    '';
+  };
+  # external interface gets IP from QEMU user stack DHCP
+  networking.interfaces."${ethExternal}".useDHCP = true;
+
+  # setup mitmproxy as transparent proxy
+  # disable ICMP redirects
+  boot.kernel.sysctl."net.ipv4.conf.all.send_redirects" = 0;
+
+  time.timeZone = "UTC";
+
+  nix.maxJobs = 1;
+  nix.buildCores = 1;
+
+  environment.systemPackages = with pkgs; [
+    screen
+    wireshark-cli
+    mitmproxy
+  ];
+  services.qemuGuest.enable = true;
+
+  systemd.services.mitmproxy = {
+    wants = ["network.target"];
+    after = ["network.target"];
+    wantedBy = ["multi-user.target"];
+    environment.SSLKEYLOGFILE = "/data/sslkeylog.dat";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''
+        ${pkgs.mitmproxy}/bin/mitmdump \
+          --mode transparent \
+          --ssl-insecure \
+          --set confdir=${mitmproxyCerts} \
+          --set stream_large_bodies=1 \
+          --showhost
+      '';
+      User = 1000;
+      Group = 1000;
+      Restart = "on-failure";
+    };
+  };
+
+  systemd.services.tshark = {
+    wants = ["network.target"];
+    after = ["network.target"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = pkgs.writeShellScript "tshark" ''
+        ${pkgs.wireshark-cli}/bin/tshark -i ${ethInternal} -w - > /data/traffic.pcap
+      '';
+    };
+  };
+
+  users.mutableUsers = false;
+  users.users.demo = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" ];
+    password = "demo";
+  };
+  users.groups.demo = {
+    gid = 1000;
+    members = ["demo"];
+  };
+
+  documentation.enable = false;
+  programs.command-not-found.enable = false;
+}
